7dd79669acda189cfab87794929226fcee975987
amitshky <amitshkymail@gmail.com>
2025-10-16 21:11:53 +0100

    updates

--- scripts/linux/homeSecondMonitorSetupI3.sh
+++ scripts/linux/homeSecondMonitorSetupI3.sh
@@ -10,6 +10,10 @@ if type "xrandr"; then
     xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-0 --off
   else
     # Otherwise, set up both displays
-    xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+    # this has been commented out because my laptop's display is acting weird
+    # xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-1-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+
+    # only displays the second monitor
+    xrandr --output eDP-1 --off --output HDMI-1-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
   fi
 fi
