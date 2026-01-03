0b7e2ffed8d7ed9c04e39b0a78fcee2b2167f037
amitshky <amitshkymail@gmail.com>
2026-01-03 17:48:02 +0000

    qt update

--- scripts/linux/cleanup.sh
+++ scripts/linux/cleanup.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
-rm -r ~/.cache/thumbnails/* 2>/dev/null
-rm ~/.local/share/recently-used.xbel 2>/dev/null
-rm ~/.local/share/qBittorrent/logs/qbittorrent.log 2>/dev/null
+rm -r $HOME/.cache/thumbnails/* 2>/dev/null
+rm $HOME/.local/share/recently-used.xbel 2>/dev/null
+rm $HOME/.local/share/qBittorrent/logs/qbittorrent.log 2>/dev/null
+pkill copyq 2>/dev/null && copyq &
