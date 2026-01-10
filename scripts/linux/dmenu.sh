9c65958c06bdeda5884630e854a87f9184a620ab
amitshky <amitshkymail@gmail.com>
2026-01-10 12:19:16 +0000

    added powermenu to dmenu script

--- scripts/linux/dmenu.sh
+++ scripts/linux/dmenu.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
 # List all executables in PATH
-all_apps=$(compgen -c)
+all_apps=$(dmenu_path)
+# all_apps=$(compgen -c)
 
 # List all Flatpak apps (only the app IDs, fast)
 flatpak_apps=$(flatpak list --app --columns=application)
+powermenu=$(printf "⏻  Shutdown\n⏾  Sleep\n  Restart\n󰍃  Logout")
 
 # Combine, sort unique, feed to dmenu
-selected=$(printf "%s\n%s\n" "$all_apps" "$flatpak_apps" | sort -u | dmenu -i -c -z 800 -l 8 -p "run:")
+selected=$(printf "%s\n%s\n%s\n" "$all_apps" "$flatpak_apps" "$powermenu" | dmenu -i -c -z 800 -l 8 -p "run:")
 
 # Run the selected app
 if [[ -n "$selected" ]]; then
     if flatpak info "$selected" &>/dev/null; then
         flatpak run "$selected" &
     else
-        nohup "$selected" &>/dev/null &
+        case "$selected" in
+          *Shutdown)  systemctl poweroff -i ;;
+          *Sleep)     slock systemctl suspend -i ;;
+          *Restart)   systemctl reboot -i ;;
+          *Logout)    pkill dwm ;;
+          *)          $selected &>/dev/null & ;;
+        esac
     fi
 fi
