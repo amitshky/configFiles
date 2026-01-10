d21222b4ec67b9e6ded5be4d0b50f556a1c48778
amitshky <amitshkymail@gmail.com>
2026-01-10 21:03:41 +0000

    update

--- scripts/linux/cleanup.sh
+++ scripts/linux/cleanup.sh
@@ -3,4 +3,5 @@
 rm -r $HOME/.cache/thumbnails/* 2>/dev/null
 rm $HOME/.local/share/recently-used.xbel 2>/dev/null
 rm $HOME/.local/share/qBittorrent/logs/qbittorrent.log 2>/dev/null
+rm /tmp/yazi* -r 2>/dev/null
 pkill copyq 2>/dev/null && copyq &
